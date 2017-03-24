4e0508820b46f78a77667f93dff88f510aa83874
Tal Bereznitskey <berzniz@gmail.com>
2017-03-24 15:00:50 +0300

    v0.0.4 (#17)
    - add tests
    - version 0.0.4 with webkit prefix

---
+++ __tests__/prefix.test.js
@@ -0,0 +1,27 @@
+import prefix from '../src/prefix';
+
+describe('prefix', () => {
+    it('should return an object on empty input', () => {
+        expect(prefix()).toEqual({});
+        expect(prefix(null)).toEqual({});
+        expect(prefix({})).toEqual({});
+    });
+
+    it('should add webkit prefix to correct properties', () => {
+        expect(prefix({
+            width: '50px',
+            transform: 1,
+            transformOrigin: 2,
+            transition: 3
+        })).toEqual({
+            width: '50px',
+            WebkitTransform: 1,
+            WebkitTransformOrigin: 2,
+            WebkitTransition: 3,
+            transform: 1,
+            transformOrigin: 2,
+            transition: 3
+        });
+    });
+
+});
--- src/prefix.js
+++ src/prefix.js
@@ -1,19 +1,21 @@
-const propertiesToPrefix = ['transform', 'transformOrigin', 'transition'];
+const propertiesToPrefix = {
+    transform: true,
+    transformOrigin: true,
+    transition: true
+};
+
 const webkitPrefix = 'Webkit';
 
-const prefix = styles => {
+const prefix = (styles) => {
     const prefixedStyles = {};
-    const styleProperties = Object.keys(styles);
-    for (let i = 0; i < styleProperties.length; i++) {
-        const property = styleProperties[i];
-        for (let i = 0; i < propertiesToPrefix.length; i++) {
-            if (propertiesToPrefix[i] === property) {
-                const prefixedProperty = webkitPrefix + property[0].toUpperCase() + property.slice(1);
-                prefixedStyles[prefixedProperty] = styles[property];
-            }
-            prefixedStyles[property] = styles[property];
+    const styleProperties = Object.keys(styles || {});
+    styleProperties.forEach(property => {
+        if (propertiesToPrefix[property]) {
+            const prefixedProperty = webkitPrefix + property[0].toUpperCase() + property.slice(1);
+            prefixedStyles[prefixedProperty] = styles[property];
         }
-    }
+        prefixedStyles[property] = styles[property];
+    });
     return prefixedStyles;
 };
 
